55d6e11b96dbc1a318b4f1144a0b0833c990324f
Peter Yates <peter.yates@graphia.co.uk>
2022-01-27 20:11:16 +0000

    Add some validation, update the readme

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,6 @@
 class Search < ApplicationRecord
+  validates :do_you_have_a_trn, presence: true, on: :find_your_trn
+  validates :name, presence: { message: "Enter your name" }, on: :what_is_your_name
+  validates :date_of_birth, presence: { message: "Enter your date of birth" }, on: :what_is_your_date_of_birth
+  validates :email, presence: { message: "Enter your email address" }, on: :where_should_we_send_your_trn
 end
